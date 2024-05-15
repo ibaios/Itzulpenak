2bfbbce1bed0e6984a9f01b70a4dd04d16872d6b
ibaios <ibai@oihanguren.com>
2024-05-15 13:34:58 +0200

    Hollow Knight instalatzaile berria

--- Hollow Knight/Instalatzailea/hollow-knight-eu.sh
+++ Hollow Knight/Instalatzailea/hollow-knight-eu.sh
@@ -2,46 +2,91 @@
 
 itzultool_version=0.4
 itzultool_filename=itzultool-"$itzultool_version"-linux-x64
+gamefolder=Hollow\ Knight/hollow_knight_Data
+repourl=https://ibaios.eus/itzulpenak
+gameurl=hollowknight
+emipprefix=hollow-knight
+tempfolder=hollow-knight-eu-instalazioa
+gamename="Hollow Knight"
+$email=ibaios@disroot.org
 
-echo "Hollow Knight euskaraz - Instalatzen..."
+echo "$gamename euskaraz - Instalatzen..."
 
 # Aukeratu gainidatziko den hizkuntza
 while true; do
-	read -p "Zein hizkuntza gainidatzi nahi duzu?
-	1: Gaztelania
-	2: Frantsesa
-	(idatzi 1 edo 2 eta sakatu SARTU tekla)
-	" esfr
-	case $esfr in
-		1 ) locale='es'; break;;
-		2 ) locale='fr'; break;;
-		* ) echo "
-		OKERREKO AUKERA. Idatzi 1 (Gaztelania) edo 2 (Frantsesa) eta sakatu SARTU tekla.
-		";;		
-	esac
+    read -p "Zein hizkuntza gainidatzi nahi duzu?
+    1: Gaztelania
+    2: Frantsesa
+    (idatzi 1 edo 2 eta sakatu SARTU tekla)
+    " esfr
+    case $esfr in
+        1 ) locale='es'; break;;
+        2 ) locale='fr'; break;;
+        * ) echo "
+        OKERREKO AUKERA. Idatzi 1 (Gaztelania) edo 2 (Frantsesa) eta sakatu SARTU tekla.
+        ";;		
+    esac
 done
 
 # Bilatu jokoaren kokalekua
-path=~/.steam/steam/steamapps/common/Hollow\ Knight/hollow_knight_Data/
+path=""
+paths=()
 
-if [[  ! -d "$path" ]]; then
-	input=~/.steam/steam/config/libraryfolders.vdf
-	while read -r line; do
-		if [[ $line == \"path\"* ]]; then
-			base=$(echo $line | cut -d '"' -f 4)
-			path="$base"/steamapps/common/Hollow\ Knight/hollow_knight_Data
-			if [[ -d "$path" ]]; then
-				break
-			fi
-		fi
-	done < "$input"
+
+steamconfigpath=~/.steam/steam/config/
+if [[ ! -d "$steamconfigpath" ]]; then
+    steamconfigpath=~/.var/app/com.valvesoftware.steam/.local/share/steam/config/
+    if [[ ! -d "$steamconfigpath" ]]; then
+        steamconfigpath=""
+        echo "EZIN IZAN DA STEAMEKO KONFIGURAZIO KARPETA AURKITU."
+    fi
+fi
+
+if [[ ! -z "$steamconfigpath" ]]; then
+    config="$steamconfigpath"/libraryfolders.vdf
+    if [[ -f "$config" ]]; then
+        while read -r line; do
+            if [[ $line == \"path\"* ]]; then
+                base=$(echo $line | cut -d '"' -f 4)
+                optpath="$base"/steamapps/common/$gamefolder
+                if [[ -d "$optpath" ]]; then
+                    paths+=("$optpath")
+                    #echo "Konfigurazioan $optpath aurkitu da..."
+                fi
+            fi
+        done < "$config"
+    fi
+
+    if [[ ${#paths[@]} > 0 ]]; then
+        if [[ ${#paths[@]} == 1 ]]; then
+            path=${paths[0]}
+        else
+            # Galdetu erabiltzaileari
+            echo "Jokoarentzako karpeta posible bat baino gehiago aurkitu dira. Zein da jokoaren benetako karpeta?"
+            select selpath in "${paths[@]}"; do
+                if [[ -z "$selpath" ]]; then
+                    printf 'Okerreko aukera.\n' "$selpath" >&2
+                else
+                    path="$selpath"
+                    break
+                fi
+            done
+        fi
+    fi
+fi
+
+if [[ -z "$path" ]] then
+    read -p "Ez da jokoaren karpeta aurkitu. Idatzi eskuz non dagoen: " path
+    while [[ ! -d "$path" ]]; do
+        read -p "Sartutako kokalekua ez da existitzen. Saiatu berriz: " path
+    done
 fi
 
 echo "Path: $path"
 
 # Instalaziorako karpeta sortu
-mkdir hollow-knight-eu-instalazioa
-cd hollow-knight-eu-instalazioa
+mkdir $tempfolder
+cd $tempfolder
 
 echo "UABE Avalonia deskargatzen..."
 
@@ -57,18 +102,33 @@ echo "Deskargatuta."
 echo "EMIP itzulpen-fitxategia deskargatzen..."
 
 # Deskargatu EMIP fitxategia
-wget https://ibaios.eus/itzulpenak/hollowknight/hollow-knight-eu-$locale.emip
+wget $repourl/$gameurl/$emipprefix-eu-$locale.emip
 
 echo "Deskargatuta."
 echo "Itzulpena aplikatzen..."
 
 # Aplikatu itzulpena
-./"$itzultool_filename" applyemip hollow-knight-eu-$locale.emip "$path"
+eginda=0
+./"$itzultool_filename" applyemip $emipprefix-eu-$locale.emip "$path" && eginda=1
 
-echo "Aplikatuta."
-echo "Instalazioko fitxategiak ezabatzen..."
-cd ..
-rm -R hollow-knight-eu-instalazioa/
+if [[ $eginda == 0 ]]; then
+    echo "Huts egin du."
+    echo "Instalazioko fitxategiak ezabatzen..."
+    cd ..
+    rm -R $tempfolder/
 
-# Instalatuta!
-echo "Instalazioa behar bezala burutu da. Orain Hollow Knight euskaraz izango duzu."
+    # Errorea
+    echo ""
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Arazoren bat gertatu da $gamename jokoaren euskaratzea instalatzean. Saiatu berriro edo idatzi $email helbidera lagun zaitzadan."
+else
+    echo "Aplikatuta."
+    echo "Instalazioko fitxategiak ezabatzen..."
+    cd ..
+    rm -R $tempfolder/
+
+    # Instalatuta!
+    echo ""
+    echo "✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔"
+    echo "Instalazioa behar bezala burutu da. Orain $gamename euskaraz izango duzu."
+fi
